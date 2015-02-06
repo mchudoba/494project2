ed5031913f97954f8a1bf3f797727ef27050e09f
Matt Chudoba <mchudoba5@gmail.com>
2015-02-06 18:01:23 -0500

    Player added, movement not working

---
+++ Prototype 1/Assets/__Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax;
+}
+
+public class Player : MonoBehaviour
+{
+	public float		speed;
+	public Boundary		boundary;
+
+	void FixedUpdate()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(horizontal, vertical, 0f);
+		rigidbody.velocity = movement * speed;
+
+		rigidbody.position = new Vector3
+		(
+			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp(rigidbody.position.y, boundary.yMin, boundary.yMax),
+			0f
+		);
+	}
+}
