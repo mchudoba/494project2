f6d5a39550e846258b41719f883daa0a757e8716
Matt Chudoba <mchudoba5@gmail.com>
2015-02-09 01:06:00 -0500

    Basic movement

--- Prototype 1/Assets/__Scripts/Player.cs
+++ Prototype 1/Assets/__Scripts/Player.cs
@@ -9,22 +9,33 @@ public class Boundary
 
 public class Player : MonoBehaviour
 {
+	private Vector3		movement = Vector3.zero;
+
 	public float		speed;
 	public Boundary		boundary;
 
+	void Update()
+	{
+		Move();
+	}
+
 	void FixedUpdate()
 	{
-		float horizontal = Input.GetAxis("Horizontal");
-		float vertical = Input.GetAxis("Vertical");
-
-		Vector3 movement = new Vector3(horizontal, vertical, 0f);
 		rigidbody.velocity = movement * speed;
-
+		
 		rigidbody.position = new Vector3
 		(
 			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
 			Mathf.Clamp(rigidbody.position.y, boundary.yMin, boundary.yMax),
 			0f
 		);
+	}
+
+	// Checks input axes and sets velocity vector
+	void Move()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		movement = new Vector3(horizontal, vertical, 0f);
 	}
 }
